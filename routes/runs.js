e0723e7c4f5da8894fc2ae60bd58a96886a1e512
Rinhard javed <rinhardjaved@gmail.com>
2022-10-04 03:36:44 -0400

    implement user interface ability based upon authentication

--- routes/runs.js
+++ routes/runs.js
@@ -5,7 +5,12 @@ const router = Router()
 import * as runsCtrl from '../controllers/runs.js'
 
 router.get('/', runsCtrl.index)
-router.get('/new', runsCtrl.newRun)
+router.get('/new', isLoggedIn, runsCtrl.newRun)
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/')
+}
 router.post('/', runsCtrl.create)
 
 export {
