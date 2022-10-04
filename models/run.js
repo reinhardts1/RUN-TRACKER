23867f1103734849e6be83aecd3f4c710f06d046
Rinhard javed <rinhardjaved@gmail.com>
2022-10-04 02:22:40 -0400

    embed comSchema in run model

--- models/run.js
+++ models/run.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose'
 
+const comSchema = new mongoose.Schema({
+  content: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
+})
+
 const runSchema = new mongoose.Schema({
   name: String,
   miles: Number,
   date: Date,
   route: String,
-  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
+  comments: [comSchema]
 
 }, {
   timestamps: true
